700a1091f2c90c3dd9e60a6eff320bacac8a7b0c
RenanKanu <renankanu@gmail.com>
2020-06-07 15:02:26 -0300

    init integration with jwt

--- src/app/controller/SessionController.js
+++ src/app/controller/SessionController.js
@@ -1,19 +1,22 @@
+import jwt from 'jsonwebtoken';
 import User from '../models/User';
 
 export default async function auth(request, response) {
   const { email, password } = request.body;
-  const user = await User.findOne({
-    where: { email },
-  });
   try {
+    const user = await User.findOne({
+      where: { email },
+    });
+    const { id } = user;
     const isMatch = await User.comparePasswords(password, user.password);
-    if (isMatch) {
-      console.log('Password is match');
-    }
-    if (!isMatch) {
+
+    if (!user || !isMatch) {
       console.log('Password not match');
+      return response.status(401).json({ error: 'User / Password not valid.' });
     }
-    return response.json(password);
+    return response.json({
+      token: jwt.sign({ userId: id }, '123', { expiresIn: '1d' }),
+    });
   } catch (error) {
     return response.status(500).json(error.message);
   }
